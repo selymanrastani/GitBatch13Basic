5bfe7648689e582b5f126649b70824d90e7629c5
selymanrastani <selyman.rastani@gmail.com>
2022-08-24 20:06:46 -0400

    temperature

---
+++ src/test/java/class01/gitBasics.java
@@ -0,0 +1,9 @@
+package class01;
+
+public class gitBasics {
+    public static void main(String[] args) {
+//        Write down a code that checks the temperature from a website and then
+//        prints out that is it a beautiful day or not
+        System.out.println("Today is a beautiful day!");
+    }
+}
