6bab4dd8ba59c18f6fc4f3a962fbfcf599af314d
selymanrastani <selyman.rastani@gmail.com>
2022-08-27 10:39:09 -0400

    three line of additional code

--- src/test/java/class01/gitBasics.java
+++ src/test/java/class01/gitBasics.java
@@ -5,5 +5,8 @@ public class gitBasics {
 //        Write down a code that checks the temperature from a website and then
 //        prints out that is it a beautiful day or not
         System.out.println("Today is not a beautiful day!");
+        System.out.println("Today is not a beautiful day!");
+        System.out.println("Today is not a beautiful day!");
+
     }
 }
